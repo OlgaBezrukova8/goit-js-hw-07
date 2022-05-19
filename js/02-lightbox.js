aa5427135ac6f0d6e6a94fd5d22ac3b5e19afb52
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-05-19 17:06:08 +0100

    Solved task 2 (gallery SimpleLightbox)

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryList = document.querySelector(".gallery");
+const markupItem = createItemOfGallery(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", markupItem);
+
+function createItemOfGallery() {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+    <li>
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`;
+    })
+    .join("");
+}
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
 
 console.log(galleryItems);
